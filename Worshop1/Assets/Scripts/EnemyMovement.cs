8e4e773e43cbc7542b2c88bad557f9dfa31e464f
mirzakhalov <mirzakhalov@mail.usf.edu>
2017-10-06 21:57:36 -0400

    Add files via upload

---
+++ Worshop1/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public GameObject player;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 distance = transform.position - player.transform.position;
+        
+        if(distance.magnitude < 3)
+        {
+            transform.position += (-distance * speed * Time.deltaTime)/distance.magnitude;
+            Debug.Log(Time.deltaTime);
+        }
+	}
+}
---
+++ Worshop1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+        //Debug.Log("This is V" + v);
+        //Debug.Log("This is H" + h);
+        //Debug.Log(transform.forward + "  " + transform.right);
+
+        transform.position = transform.position + transform.forward * v * Time.deltaTime * speed;
+        transform.position = transform.position + transform.right * h * Time.deltaTime * speed;
+
+    }
+}
